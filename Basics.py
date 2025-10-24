25cc2cc43a30d66d0b2933aa0c364bf05292007c
Do Ngo <bahuynh619@gmail.com>
2025-10-25 04:00:58 +0700

    Create Basics.py

---
+++ Basics.py
@@ -0,0 +1,44 @@
+1.1/ 
+
+name = input("Enter your name : ")
+print(f"Hello, {name}") //ép kiểu để hiện tên mà người dùng nhập
+print("Welcome to Python") //input 1.1
+
+Enter your name : Do Ngo
+Hello, Do Ngo
+Welcome to Python   //Example output 1.1
+
+1.2/
+
+name = input("Enter your name: ")
+age = int(input ("Enter your age:"))
+print(f"Hi {name}, you are {age} years old") //input 1.2 //ép kiểu để hiện tên, tuổi theo đúng như người dùng nhập
+
+Enter your name: Do Ngo
+Enter your age:19
+Hi Do Ngo, you are 19 years old //Example output 1.2
+
+1.3/
+
+fnum = int(input ("Enter your first number: "))
+snum = int(input ("Enter your second number: "))
+print("Sum of them: " , fnum + snum)
+print("Difference of them: " , fnum - snum)
+print("Production of them: " , fnum * snum)
+print(f"Normal division of them: {fnum / snum:.2f} ") //ép kiểu số thực, làm tròn đến 2 chữ số thập phân, in ra phép chia bình thường
+print("Integer division of them: " , fnum // snum) // in ra phép chia lấy phần nguyên 
+
+//input 1.3
+
+Enter your first number: 4
+Enter your second number: 3
+Sum of them:  7
+Difference of them:  1
+Production of them:  12
+Normal division of them: 1.33 
+Integer division of them:  1 //Example output 1.3
+
+
+
+
+
